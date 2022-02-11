7ed2bab6558b598753abc1f5d1592ece909e9b64
Viktor <example@gmail.com>
2022-02-12 01:31:57 +0300

    sixth task

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ for (let i = 100; i >= 1; i--)  {
 
 
 // 3. Выведите с помощью цикла столбец четных чисел от 1 до 100
-let ul = document.createElement('ul');
+/* let ul = document.createElement('ul');
 document.body.append(ul);
 
 
@@ -37,3 +37,33 @@ for (let i = 2; i <= 100; i += 2)  {
     ul.append(li); 
     
 }
+ */
+
+//  4. Заполните массив 10-ю иксами с помощью цикла
+
+/* const arr = [];
+
+for( let i = 0; i <= 10; i++){
+    arr[i] = 'x';
+}
+console.log(arr); 
+*/
+
+// 5. Заполните массив числами от 1 до 10 с помощью цикла
+
+/* const arr = [];
+
+for( let i = 1; i <= 10; i++){
+    arr.push = i;
+}
+console.log(arr); 
+*/
+
+// 6. Заполните массив 10-ю случайными числами (дробями) от 0 до 1 с помощью цикла. Дроби округляйте до двух знаков в дробной части
+
+const arr = [];
+for (let i = 1; i <= 10; i++) {
+	arr.push(Math.random());
+}
+console.log(arr);
+
